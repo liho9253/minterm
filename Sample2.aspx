0b648364f4774819b3b6090cc7b8a2b448d640a8
liho9253 <timmy89566@gmail.com>
2021-11-07 00:03:12 +0800

    Sample_0413

---
+++ Sample2.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Sample2.aspx.cs" Inherits="minterm.Sample2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>訂貨系統</h2>
+            <asp:DropDownList ID="ddl_Area" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddl_Area_SelectedIndexChanged">
+            </asp:DropDownList>
+            <br/>
+            <asp:DropDownList ID="ddl_Place" runat="server"></asp:DropDownList>
+            <br/>
+            <asp:Label ID="Label1" runat="server" Text="姓名"></asp:Label>
+            <asp:TextBox ID="tb_Name" runat="server"></asp:TextBox>
+            <br/>
+            <asp:Label ID="Label2" runat="server" Text="其他"></asp:Label>
+            <br />
+            <asp:RadioButtonList ID="rbl_Res" runat="server" AutoPostBack="True">
+                <asp:ListItem Selected="True" Value="Rad_No">否</asp:ListItem>
+                <asp:ListItem Value="Rad_Yes">是</asp:ListItem>
+            </asp:RadioButtonList>
+            <asp:TextBox ID="tb_Des" runat="server" Width="400px" Visible="False"></asp:TextBox>
+            <br/>
+            <asp:Button ID="btn_Sub" runat="server" Text="送出" OnClick="btn_Sub_Click" />
+            <br />
+            <asp:Label ID="lb_Msg" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Sample2.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace minterm{
+    public partial class Sample2 : System.Web.UI.Page{
+        String[,] sa_Place = 
+            new string[2, 3] { { "基隆","台北","新北"}, { "苗栗","台中","南投"} };
+        protected void Page_Load(object sender, EventArgs e){
+            if(IsPostBack == false) { 
+                ListItem o_li = new ListItem("北區", "北區");
+                ddl_Area.Items.Add(o_li);
+                o_li = new ListItem("中區", "中區");
+                ddl_Area.Items.Add(o_li);
+
+                for (int i = 0; i <= 2;i++) {
+                    ListItem o_li2 = new ListItem(sa_Place[0, i], sa_Place[0, i]);
+                    ddl_Place.Items.Add(o_li2);
+                }
+            }
+            if (rbl_Res.Text == "Rad_Yes") 
+                tb_Des.Visible = true;
+            else
+                tb_Des.Visible = false;
+        }
+
+        protected void ddl_Area_SelectedIndexChanged(object sender, EventArgs e){
+            ddl_Place.Items.Clear();
+            ListItem o_li2;
+            if (ddl_Area.SelectedValue == "北區") { 
+                for (int i = 0; i <= 2;i++) {
+                    o_li2 = new ListItem(sa_Place[0, i], sa_Place[0, i]);
+                    ddl_Place.Items.Add(o_li2);
+                }
+            }else{
+                for (int i = 0; i <= 2;i++) {
+                    o_li2 = new ListItem(sa_Place[1, i], sa_Place[1, i]);
+                    ddl_Place.Items.Add(o_li2);
+                }
+            }
+        }
+
+        protected void btn_Sub_Click(object sender, EventArgs e){
+                String temp = "";
+                temp += ddl_Area.Text+ "<br/>";
+                temp += ddl_Place.Text + "<br/>";
+                temp += tb_Name.Text + "<br/>";
+                temp += tb_Des.Text + "<br/>";
+            lb_Msg.Text = temp;
+        }
+    }
+}
